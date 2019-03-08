eb1466d84f574f2d97bfded90b9b89d454370c93
mohammed03 <38572531+mohammed11111111@users.noreply.github.com>
2019-03-07 18:21:52 -0800

    ss

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,8 @@
-import React, { Component, lazy, Suspense } from "react";
-import ImagePlaceHolder from "./ImagePlaceHolder";
+import React, { Component } from "react";
+
+import ProjectCard from "./ProjectCard";
 import { trackWindowScroll } from "react-lazy-load-image-component";
-const ProjectCard = lazy(() => import("./ProjectCard"));
+
 class Projects extends Component {
   state = {};
   render() {
@@ -9,94 +10,46 @@ class Projects extends Component {
       <section className="projects">
         <div className="row text-center">
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                projectName="Instructor Social Network"
-                imageSrc="/images/projects/isn.png"
-                link="https://instructorscoialnetwork.herokuapp.com"
-              />
-            </Suspense>
+            <ProjectCard
+              projectName="Instructor Social Network"
+              imageSrc="/images/projects/isn.png"
+              link="https://instructorscoialnetwork.herokuapp.com"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                projectName="e-mall egypt"
-                link="https://emallegy.herokuapp.com/"
-                imageSrc="/images/projects/emall.png"
-              />
-            </Suspense>
+            <ProjectCard
+              projectName="E-Mall"
+              link="https://emallegy.herokuapp.com/"
+              imageSrc="/images/projects/emall.png"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                projectName="speech to code"
-                imageSrc="/images/projects/speechToCode.png"
-                link="https://speechtocode.herokuapp.com/"
-              />
-            </Suspense>
+            <ProjectCard
+              projectName="Speech To Code"
+              imageSrc="/images/projects/speechToCode.png"
+              link="https://speechtocode.herokuapp.com/"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                projectName="chatbotsa"
-                imageSrc="/images/projects/chatbotsa.png"
-                link="https://chatbotsatest.herokuapp.com/"
-              />
-            </Suspense>
+            <ProjectCard
+              projectName="Chatbotsa"
+              imageSrc="/images/projects/chatbotsa.png"
+              link="https://chatbotsatest.herokuapp.com/"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                link="https://womensfashion.herokuapp.com/"
-                projectName="women's fashion"
-                imageSrc="/images/projects/women.png"
-              />
-            </Suspense>
+            <ProjectCard
+              link="https://womensfashion.herokuapp.com/"
+              projectName="Women's Fashion"
+              imageSrc="/images/projects/women.png"
+            />
           </div>
           <div className="col-12 col-md-6">
-            <Suspense
-              fallback={
-                <div>
-                  <ImagePlaceHolder />
-                </div>
-              }
-            >
-              <ProjectCard
-                link="https://dscytest.herokuapp.com/"
-                projectName="Discy"
-                imageSrc="/images/projects/discy.png"
-              />
-            </Suspense>
+            <ProjectCard
+              link="https://dscytest.herokuapp.com/"
+              projectName="Discy"
+              imageSrc="/images/projects/discy.png"
+            />
           </div>
         </div>
       </section>
